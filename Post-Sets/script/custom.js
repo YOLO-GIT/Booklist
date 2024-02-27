8a06e043bfd49a29bd96eab434b273bab1b64408
YOLO-GIT <ahmadsufi345@gmail.com>
2024-02-27 15:39:38 +0800

    Fix some Functionalities

--- Post-Sets/script/custom.js
+++ Post-Sets/script/custom.js
@@ -1,8 +1,38 @@
-function myFunction() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-        x.type = "text";
+const showPasswordIcon = document.getElementById('showPassword');
+const passwordInput = document.getElementById('password');
+const confirmpassword = document.getElementById('confirm-password');
+const eyeIcon = document.getElementById('eyeIcon');
+const eyeSlashIcon = document.getElementById('eyeSlashIcon');
+
+showPasswordIcon.addEventListener('click', function () {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.style.display = 'none';
+        eyeSlashIcon.style.display = 'block';
+        console.log("Bruh one");
     } else {
-        x.type = "password";
+        passwordInput.type = 'password';
+        eyeIcon.style.display = 'block';
+        eyeSlashIcon.style.display = 'none';
+        console.log("Bruh two");
     }
-}
+});
+
+const showConfirmPasswordIcon = document.getElementById('showConfirmPassword');
+const confirmPasswordInput = document.getElementById('confirm-password');
+const eyeIconConfirm = showConfirmPasswordIcon.querySelector('#eyeIcon');
+const eyeSlashIconConfirm = showConfirmPasswordIcon.querySelector('#eyeSlashIcon');
+
+showConfirmPasswordIcon.addEventListener('click', function () {
+    if (confirmPasswordInput.type === 'password') {
+        confirmPasswordInput.type = 'text';
+        eyeIconConfirm.style.display = 'none';
+        eyeSlashIconConfirm.style.display = 'block';
+        console.log("bruh bro");
+    } else {
+        confirmPasswordInput.type = 'password';
+        eyeIconConfirm.style.display = 'block';
+        eyeSlashIconConfirm.style.display = 'none';
+        console.log("bruh hoi");
+    }
+});
