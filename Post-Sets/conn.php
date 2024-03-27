602bdd6c2ad37fe7e15eb3f14d28e257e17d12af
YOLO-GIT <ahmadsufi345@gmail.com>
2024-03-27 13:02:20 +0800

    Added Connection

---
+++ Post-Sets/conn.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "id21576521_root";
+$pass = "LibraryPro1234+5";
+$db = "id21576521_dbopac";
+
+$con = mysqli_connect($host, $user, $pass, $db);
+
+if (!$con) {
+    die("Database failed");
+}
+
+// Connection mg lah jim
